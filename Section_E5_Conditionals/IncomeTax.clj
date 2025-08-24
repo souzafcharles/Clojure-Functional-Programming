b02b4b3e994dd441840e1e9ad2b0077bbcf2de85
Charles Fernandes de Souza <souzach@outlook.com>
2025-08-24 19:12:54 -0300

    refactor: Update salary calculation logic

--- Section_E5_Conditionals/IncomeTax.clj
+++ Section_E5_Conditionals/IncomeTax.clj
@@ -1,11 +1,16 @@
-(defn income-tax?
-  [salary]
-  (let [TAX-FIRST-BAND 0
-        TAX-SECOND-BAND 0.1
-        TAX-THIRD-BAND 0.2]
-    (cond
-      (<= salary 1000) (* salary TAX-FIRST-BAND)
-      (<= salary 2000) (* salary TAX-SECOND-BAND)
-      :else            (* salary TAX-THIRD-BAND))))
+(def extra-tax-10 0.10)
+(def extra-tax-20 0.20)
 
-(println (format "%.2f" (income-tax? 1500.00)))
+(defn calculate-salary [salary]
+  (cond
+    (<= salary 1000) salary
+    (< salary 2000) (+ salary (* salary extra-tax-10))
+    :else (+ salary (* salary extra-tax-20))))
+
+(defn display-salary [salary]
+  (println (format "Salary Amount (GBP): %.2f" (calculate-salary salary))))
+
+;; Examples
+(display-salary 1000.00)
+(display-salary 1500.00)
+(display-salary 2000.00)
